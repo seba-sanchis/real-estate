f33b7e642caae57acaf6eb43556e07816442de8d
seba-sanchis <seba.sanchis@gmail.com>
2024-12-23 15:26:34 -0300

    details screen

--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -149,3 +149,17 @@ export async function getProperties({
     return [];
   }
 }
+
+export async function getPropertyById({ id }: { id: string }) {
+  try {
+    const result = await databases.getDocument(
+      config.databaseId!,
+      config.propertiesCollectionId!,
+      id
+    );
+    return result;
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+}
